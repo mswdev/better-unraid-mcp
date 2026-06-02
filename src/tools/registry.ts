17b40766c6253d50e95b45628f54dff2e6fbbd75
Matt White <matt@msw.dev>
2026-06-01 23:02:56 -0600

    feat(docker): add docker_container_list read tool

--- src/tools/registry.ts
+++ src/tools/registry.ts
@@ -3,6 +3,7 @@ import type { GraphQLExecutor } from "../graphql/client.js";
 import { registerArrayStatus } from "./array/array-status.js";
 import { registerParityHistory } from "./array/parity-history.js";
 import { registerDiskList } from "./disk/disk-list.js";
+import { registerDockerContainerList } from "./docker/container-list.js";
 import { registerShareList } from "./share/share-list.js";
 import { registerSystemInfo } from "./system/system-info.js";
 
@@ -19,4 +20,5 @@ export function registerAllTools(server: McpServer, client: GraphQLExecutor): vo
   registerParityHistory(server, client);
   registerDiskList(server, client);
   registerShareList(server, client);
+  registerDockerContainerList(server, client);
 }
